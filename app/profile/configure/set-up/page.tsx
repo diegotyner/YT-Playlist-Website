75b18c5c5cd4b9781116d6c34b6452442cd10e2d
diegotyner <120358477+diegotyner@users.noreply.github.com>
2024-08-20 01:53:56 -0700

    deploy?

--- app/profile/configure/set-up/page.tsx
+++ app/profile/configure/set-up/page.tsx
@@ -4,7 +4,7 @@ const SetUp = async () => {
   console.log("User setting up")
   return (
     <div className="border-gray-600 border-b-2">
-      It looks like you haven't set your user display preferences yet! Before you can submit playlists, please select a display name and profile picture
+      It looks like you haven&apos;t set your user display preferences yet! Before you can submit playlists, please select a display name and profile picture
     </div>
   );
 };
--- components/playlist-card.tsx
+++ components/playlist-card.tsx
@@ -12,7 +12,7 @@ const playlistCard = ({avator_url, creator_name, playlist_id, title, videos}: pl
   return (
     <div className='w-[700px]'>
       <Link href={`view/${playlist_id}`} className='grid grid-cols-5 pt-5 hover:bg-slate-200'>
-        {videos.map(item => (<img src={item}/>))}
+        {videos.map((item, index) => (<img key={index} src={item}/>))}
       </Link>
       <div className='flex flex-col'>
         <h1>{title}</h1>
