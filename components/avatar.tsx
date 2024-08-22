c92cf643b8a384de7f11660c4cc0077b6134c44c
diegotyner <120358477+diegotyner@users.noreply.github.com>
2024-08-22 13:53:26 -0700

    Fun card update

---
+++ components/avatar.tsx
@@ -0,0 +1,18 @@
+import * as Avatar from '@radix-ui/react-avatar';
+
+interface CustomAvatarProps {
+  avatar_url?: string;
+  backup?: string;
+}
+const CustomAvatar = ({avatar_url, backup}:CustomAvatarProps)  => {
+  return (
+    <Avatar.Root className="bg-blackA1 inline-flex h-[75px] w-[75px] select-none items-center justify-center overflow-hidden rounded-full align-middle">
+      <Avatar.Image src={avatar_url} className='h-full w-full rounded-[inherit] object-cover border-2 border-neutral-400' alt='Profile Picture'/>
+      <Avatar.Fallback className="flex h-full w-full items-center justify-center rounded-[inherit] bg-white text-[15px] border-2 border-neutral-400" delayMs={1}>
+        <img src={backup ? backup : '/icons/add_pfp.svg'} className='h-10/12 w-10/12 object-cover pl-1 opacity-70'/>
+      </Avatar.Fallback>
+  </Avatar.Root>
+  )
+}
+
+export default CustomAvatar
--- components/playlist-card.tsx
+++ components/playlist-card.tsx
@@ -1,77 +1,101 @@
-
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import '@/components/playlist-card.css';
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import CustomAvatar from "./avatar";
+import "@/components/playlist-card.css";
 
 interface playlistCardProps {
-  avator_url?: string;
+  avatar_url?: string;
   creator_name?: string;
   playlist_id: string;
   title: string;
   videos: string[];
 }
-const PlaylistCard = ({avator_url, creator_name, playlist_id, title, videos}: playlistCardProps) => {
-  const [deleted, setDeleted] = useState(false)
+const PlaylistCard = ({
+  avatar_url,
+  creator_name,
+  playlist_id,
+  title,
+  videos,
+}: playlistCardProps) => {
+  const [deleted, setDeleted] = useState(false);
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-  
+
   const handleDelete = async () => {
-    const hasConfirmed = confirm("Are you sure you want to delete this playlist?");
-    if (!hasConfirmed) return
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this playlist?"
+    );
+    if (!hasConfirmed) return;
 
     const response = await fetch(`/api/delete/${playlist_id}`, {
-        method: "DELETE"
-    })
-    console.log(response)
+      method: "DELETE",
+    });
+    console.log(response);
     if (!response.ok) {
-      alert('deletion failed')
-      return
+      alert("deletion failed");
+      return;
     }
-    setDeleted(true)
-  } 
+    setDeleted(true);
+  };
 
   if (deleted) return null;
 
   return (
     <>
-      <div className='w-[400px] border-2 border-neutral-400 p-3 rounded-2xl'>
-
-        
-        <div className='mt-5 h-[200px]'>    
-          <Link href={`view/${playlist_id}`} className='w-full h-full inset-attempt bg-gray-800 clickable py-6 rounded-2xl'>
-            {videos.map((item, index) => (<>
-              <img className={`border-2 border-gray-400 showing-${index}`} key={index} src={item} 
-                onMouseEnter={() => setHoveredIndex(index)}
-                onMouseLeave={() => setHoveredIndex(null)}
-              />
-              <img className={`border-2 border-gray-400 backup hidden-${index} ${index === hoveredIndex ? `hovered hovered-${index}` : ''}`} key={`${index}_hidden`} src={item}
-
-              />
-              </>))}
+      <div className="w-[400px] border-2 border-neutral-400 p-3 rounded-2xl">
+        <div className="h-[200px]">
+          <Link
+            href={`view/${playlist_id}`}
+            className="w-full h-full inset-attempt bg-gray-800 clickable py-6 rounded-2xl"
+          >
+            {videos.map((item, index) => (
+              <>
+                <img
+                  className={`border-2 border-gray-400 showing-${index}`}
+                  key={index}
+                  src={item}
+                  onMouseEnter={() => setHoveredIndex(index)}
+                  onMouseLeave={() => setHoveredIndex(null)}
+                />
+                <img
+                  className={`border-2 border-gray-400 backup hidden-${index} ${
+                    index === hoveredIndex ? `hovered hovered-${index}` : ""
+                  }`}
+                  key={`${index}_hidden`}
+                  src={item}
+                />
+              </>
+            ))}
           </Link>
         </div>
 
         {/* Home view */}
-        { (avator_url && creator_name) && 
-          <div className='flex flex-col'>
-            <h1 className='truncate'>{title}</h1>
-            <div className='flex items-center'>
-              <img src={avator_url} width={100}/>
-              <p>{creator_name}</p>
+        {(avatar_url || creator_name) && (
+          <div className="flex w-auto mx-2 mt-2">
+            <CustomAvatar avatar_url={avatar_url} />
+            <div className="flex-grow flex flex-col w-1 pl-3 my-2">
+              <h1 className="w-full truncate">{title}</h1>
+              <p className="text-gray-600">{creator_name}</p>
             </div>
-          </div> }
+          </div>
+        )}
 
         {/* Profile view */}
-        { !(avator_url && creator_name) && 
-          <div className='flex items-center justify-between'>
+        {!(avatar_url || creator_name) && (
+          <div className="flex items-center justify-between">
             <h1>{title}</h1>
-            <img src="icons/trash.svg" alt="wrench icon" className="clickable" width={30} onClick={handleDelete}></img>
-         </div>}
+            <img
+              src="icons/trash.svg"
+              alt="wrench icon"
+              className="clickable"
+              width={30}
+              onClick={handleDelete}
+            ></img>
+          </div>
+        )}
       </div>
-
-
-  </>
-  )
-}
+    </>
+  );
+};
 
 /* <Link href={`view/${playlist_id}`} className='grid grid-cols-5 pt-5 hover:bg-slate-200'>
   {videos.map((item, index) => (<img className="border-2 border-black" key={index} src={item} width={120}/>))}
@@ -82,4 +106,4 @@ const PlaylistCard = ({avator_url, creator_name, playlist_id, title, videos}: pl
   </Link>
 </div> */
 
-export default PlaylistCard
+export default PlaylistCard;
